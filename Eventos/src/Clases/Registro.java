5dcac37bf32daa834991ab40637ab69846dfa12c
Yampier Sanchez <U23232824@utp.edu.pe>
2024-10-24 23:50:30 -0500

    Ya termine de subir las clases q faltaban (Yampier Sanchez)

---
+++ Eventos/src/Clases/Registro.java
@@ -0,0 +1,16 @@
+
+package Clases;
+
+public class Registro {
+    private int codigoRegistro; 
+    private String fechaRegistro;
+    private int codigoParticipante;
+    private int codigoEvento;
+    private String estado;
+    
+    public void validarRegistro() {
+    }
+    
+    public void confirmarAsistencia() {
+    }
+}
---
+++ Eventos/src/Clases/Ubicacion.java
@@ -0,0 +1,11 @@
+
+package Clases;
+
+public class Ubicacion {
+    private String direccion; 
+    private String ciudad; 
+    private String pais;
+    
+    public void asignarUbicacion() {
+    }
+}
