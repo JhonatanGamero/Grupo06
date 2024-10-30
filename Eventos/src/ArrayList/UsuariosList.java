1a1d15ed2c59f6fc2f339c36541087c434d83d88
JhonatanGamero <Alejandrogameroreyes@gmail.com>
2024-10-29 22:38:50 -0500

    Ya realice mi ArrayList (Gamero Jhonatan)

---
+++ Eventos/src/ArrayList/UsuariosList.java
@@ -0,0 +1,26 @@
+
+package ArrayList;
+
+import Clases.Usuarios;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuariosList {
+    List<Usuarios> listaUsuarios = new ArrayList<>();
+    
+    public void agregar (Usuarios u){
+        listaUsuarios.add(u);
+    }
+    
+    public void quitar (Usuarios u){
+         listaUsuarios.remove(u);
+    }
+    
+    public Usuarios obtener(int pos){
+        return listaUsuarios.get(pos);
+    }
+    
+    public List<Usuarios> listar (){
+         return this.listaUsuarios;
+    }
+}
