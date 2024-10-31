d9b56ada88df9226657ab48ff81a09c753ec0e0e
Yampier Sanchez <U23232824@utp.edu.pe>
2024-10-30 23:31:01 -0500

    Realice algunos cambios (Yampier Sanchez)

--- Eventos/src/ArrayList/OrganizadorList.java
+++ Eventos/src/ArrayList/OrganizadorList.java
@@ -1,21 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ArrayList;
 
 import Clases.Organizador;
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- *
- * @author Administrator
- */
 public class OrganizadorList {
     
-   
     List<Organizador> listaOrganizador = new ArrayList<>();
     
     public void agregar (Organizador o){
